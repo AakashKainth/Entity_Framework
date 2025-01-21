c1c65dcb040f752bcaca905eaf8afdbfce1583d6
AakashKainth <aakash.kainth@gmail.com>
2025-01-20 21:21:46 -0500

    Add project files.

---
+++ Entity_Framework/EfDemoDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Entity_Framework
+{
+    public class EfDemoDbContext : DbContext
+    {
+        private readonly string _connString;
+        public DbSet<Product> Products { get; set; }
+
+        public EfDemoDbContext(string connString)
+        {
+            _connString = connString;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_connString);
+            }
+        }
+    }
+}
---
+++ Entity_Framework/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Entity_Framework
+{
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var connectionString = "Server=LAPTOP-97QETT33\\WINTER2022;Database=EfDemo;Trusted_Connection=True;TrustServerCertificate=True;";
+            using (var context = new EfDemoDbContext(connectionString))
+            {
+                Console.WriteLine($"Adding new Product");
+                var productToADD = new Product
+                {
+                    Name = "New Product",
+                    Description = "New Product Description"
+                };
+                context.Products.Add(productToADD);
+            }
+
+
+
+
+        }
+
+    }
+}
